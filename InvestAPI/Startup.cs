3cb59fe8592e1cb5c228accc366dc797afb7b311
Rafael Cavalcante <rafael.cavalcante@pitang.com>
2022-01-13 23:56:48 -0300

    Retirada do Swagger

--- InvestAPI/Startup.cs
+++ InvestAPI/Startup.cs
@@ -22,13 +22,7 @@ namespace InvestAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            //services.AddDbContext<DataContext>(opt => opt.UseNpgsql("Host=ec2-18-211-41-246.compute-1.amazonaws.com;Database=daqu3skamvcq6t.SafraInvest;Username=hhcjncmqsdgdpy;Password=9d2711737db14d0a19382076a094e663be9c96a0cf4a07ce39d67aa33beeddbd;sslmode=on"));
-            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Safra"));
             services.AddDbContext<DataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("Default")));
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API Teste BackEnd Developer", Version = "" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +31,6 @@ namespace InvestAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InvestAPI v1"));
             }
 
             app.UseHttpsRedirection();
